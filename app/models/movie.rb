aa58ba974891f513cbc509b434bc031fdea1c5cc
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-02-24 19:09:53 +0100

    CRUD - Showing a Single Movie

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,6 +4,4 @@ class Movie < ApplicationRecord
     validates :where_to_watch, presence: true
     validates :runtime, presence: true, numericality: { only_integer: true, greater_than: 0 }
     validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
-    validates :url, presence: true, url: true
-    validates :picture_url, presence: true, url: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   root "movies#index"
 
   get "/movies", to: "movies#index"
+  get "/movies/:id", to: "movies#show"
 
 end
