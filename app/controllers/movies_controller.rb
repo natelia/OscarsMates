8a1d2acba30c1330e8e46e6b7b52c33fdecb6a2d
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-02-27 21:20:26 +0100

    CRUD - Creating a New Movie

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,4 +7,23 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def new 
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+     
+    if @movie.save
+      redirect_to @movie
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def movie_params
+      params.require(:movie).permit(:title, :english_title, :where_to_watch, :runtime, :rating)
+    end
 end
