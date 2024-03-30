61f5fdc3bf58adf70e8a7d1031158cb3f2bdfca4
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-03-30 22:24:59 +0100

    Initial commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,10 @@
 class Movie < ApplicationRecord
 
-  def self.released
-    Movie.where("released_on < ?", Time.now).order("released_on desc")
-  end
 
-  validates :title, presence: true
-  validates :english_title, presence: true
-  validates :where_to_watch, presence: true
+
+  validates :title, :english_title, :where_to_watch, :released_on, presence: true
   validates :runtime, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
   validates :url, presence: true
   validates :picture_url, presence: true
-  validates :released_on, presence: true
 end
