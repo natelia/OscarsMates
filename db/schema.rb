8f88528d942b7600a04e7743e5bdf1b8af3eb793
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-04-03 17:39:19 +0200

    Rollback wrong migration

--- db/schema.rb
+++ db/schema.rb
@@ -21,7 +21,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_26_213359) do
     t.string "picture_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.date "released_on"
   end
 
 end
