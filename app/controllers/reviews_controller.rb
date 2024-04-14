a314a52c9f9b2082e93556ea60e4ec459b38c7df
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-04-14 17:14:38 +0200

    Implement the Create Action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,4 @@
 class ReviewsController < ApplicationController
-
   def index
     @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
@@ -9,4 +8,23 @@ class ReviewsController < ApplicationController
     @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
+
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie),
+                  notice: "Thanks for your review!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:name, :comment, :stars)
+  end
+
 end
