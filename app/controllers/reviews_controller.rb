e24d49da00367d35e9819323a98751ef7f0a0dcd
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-04-14 17:06:45 +0200

    Create the New Action and Template

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,12 @@
+class ReviewsController < ApplicationController
+
+  def index
+    @movie = Movie.find(params[:movie_id])
+    @reviews = @movie.reviews
+  end
+
+  def new
+    @movie = Movie.find(params[:movie_id])
+    @review = @movie.reviews.new
+  end
+end
