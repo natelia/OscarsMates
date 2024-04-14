f915139be096424a79250f86180ffda2d8cccc6b
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-04-14 17:47:04 +0200

    Refactor

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,14 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
 
     if @review.save
@@ -27,4 +25,7 @@ class ReviewsController < ApplicationController
     params.require(:review).permit(:name, :comment, :stars)
   end
 
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
