e71aff68e397a32f8f644462e80224522b42050c
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-04-29 16:06:17 +0200

    Edit User Account Information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,16 +14,28 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to @user, notice: "Thanks for signing up!"
+      redirect_to @user, notice: 'Thanks for signing up!'
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: 'Account successfully updated!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 private
 
-def user_params
-  params.require(:user).permit(:name, :email, :password, :password_confirmation)
-end
-
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
