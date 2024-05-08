09e00aceca30f77a6190204accf630e5e7e2e855
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-05-08 20:34:55 +0200

    Generated a 'Sign Out' Link and Implemented the Destroy Action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,5 +14,8 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to movies_url, status: :see_other,
+      notice: "You're now signed out!"
   end
 end
