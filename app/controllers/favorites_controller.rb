1587bdcac94550e5f8795328674c4243ab27c309
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-09 15:24:20 +0200

    Implement the Create Action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,9 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+    def create
+      @movie = Movie.find(params[:movie_id])
+      @movie.favorites.create!
+
+      redirect_to @movie
+    end
 end
