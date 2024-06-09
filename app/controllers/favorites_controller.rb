e9814544f2ef0e11054c81338b09c5072b749236
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-09 15:49:16 +0200

    Only Allow One Fave Per User

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,9 @@
 class FavoritesController < ApplicationController
   before_action :require_signin
-    def create
-      @movie = Movie.find(params[:movie_id])
-      @movie.favorites.create!
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @movie.favorites.create!
 
-      redirect_to @movie
-    end
+    redirect_to @movie
+  end
 end
