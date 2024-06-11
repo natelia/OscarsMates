0f6fe52e7386ec151fa9023bb5cac5d6c87ace90
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-11 13:32:01 +0200

    Generate the Genre Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_30_204148) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_11_110823) do
   create_table "favorites", force: :cascade do |t|
     t.integer "movie_id", null: false
     t.integer "user_id", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_30_204148) do
     t.datetime "updated_at", null: false
     t.index ["movie_id"], name: "index_favorites_on_movie_id"
     t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "movies", force: :cascade do |t|
