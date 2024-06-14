65004b197bc3cada2a845ff1d2c2ec96f2440b88
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-06-14 23:22:04 +0200

    Generate the Characterization Join Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_11_110823) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_14_211451) do
+  create_table "characterizations", force: :cascade do |t|
+    t.integer "movie_id", null: false
+    t.integer "genre_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["genre_id"], name: "index_characterizations_on_genre_id"
+    t.index ["movie_id"], name: "index_characterizations_on_movie_id"
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "movie_id", null: false
     t.integer "user_id", null: false
@@ -57,6 +66,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_11_110823) do
     t.boolean "admin", default: false
   end
 
+  add_foreign_key "characterizations", "genres"
+  add_foreign_key "characterizations", "movies"
   add_foreign_key "favorites", "movies"
   add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "movies"
