acd6e41142b9e28f1784b1b16af01966ac0c81da
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-09-02 21:53:26 +0200

    Add Review Validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -9,4 +9,13 @@ class Review < ApplicationRecord
     in: STARS,
     message: 'must be between 1 and 5'
   }
+  validate :unique_user_review_for_movie
+
+  private
+
+  def unique_user_review_for_movie
+    if Review.exists?(user_id: user_id, movie_id: movie_id)
+      errors.add(:base, 'You have already reviewed this movie')
+    end
+  end
 end
