36f344814338c8ba8f50fa74f01c930b09d8c7df
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-09-21 23:56:43 +0200

    Add Routes for Follow and Unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,16 @@ Rails.application.routes.draw do
   resources :categories
   resources :nominations
   resources :genres
-  resources :users
-  root 'movies#index'
-  resources :movies do
+  resources :users do
+    member do
+      post 'follow', to: 'follows#create'
+      delete 'unfollow', to: 'follows#destroy'
+    end
+  end
 
+  root 'movies#index'
+
+  resources :movies do
     resources :reviews
     resources :favorites, only: [:create, :destroy]
   end
