d6b39d045e660ea7d471e65dc86bf8f522e0af21
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-09-22 22:59:16 +0200

    Add Followers and Following Count to User Profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,11 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorite_movies, through: :favorites, source: :movie
-  
+
+  # Users that the current user is following
   has_many :follows, foreign_key: :follower_id, dependent: :destroy
   has_many :following, through: :follows, source: :followed
+  # Users that follow the current user
   has_many :inverse_follows, class_name: 'Follow', foreign_key: :followed_id, dependent: :destroy
   has_many :followers, through: :inverse_follows, source: :follower
 
