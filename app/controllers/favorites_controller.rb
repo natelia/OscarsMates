1662b284376fdc66c935a4a7415a40e3e3b58ae0
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-10-20 22:47:24 +0200

    Refactor FavoritesController to eliminate redundancy and improve readability

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,18 +2,23 @@
 # It allows users to mark movies as favorites and remove them from their list of favorites.
 class FavoritesController < ApplicationController
   before_action :require_signin
-  def create
-    @movie = Movie.find_by!(slug: params[:movie_id])
-    @movie.favorites.create!(user: current_user)
+  before_action :set_movie
 
-    redirect_to @movie
+  def create
+    @movie.favorites.create!(user: current_user)
+    redirect_to @movie, notice: 'Movie was successfully added to favorites.'
   end
 
   def destroy
-    favorite = current_user.favorites.find(params[:id])
+    favorite = current_user.favorites.find_by!(movie: @movie)
     favorite.destroy
+    redirect_to @movie, notice: 'Movie was successfully removed from favorites.'
+  end
 
+  private
+
+  # Sets the movie instance variable for create and destroy actions.
+  def set_movie
     @movie = Movie.find_by!(slug: params[:movie_id])
-    redirect_to @movie
   end
 end
