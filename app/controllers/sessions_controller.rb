544b0e8d0e3f5f7f5edfbd5308f576657ea1abf1
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-10-25 23:37:53 +0200

    Refactor SessionController to improve readability and modularity

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,34 +3,31 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = find_user_and_authenticate
+    user = authenticate_user
 
-    if user
-      handle_successful_authentication(user)
-    else
-      handle_failed_authentication
-    end
-  end
-
-  def find_user_and_authenticate
-    User.find_by(email: params[:email])&.authenticate(params[:password])
-  end
-
-  def handle_successful_authentication(user)
-    session[:user_id] = user.id
-    redirect_to (session[:intended_url] || user),
-                notice: "Welcome back, #{user.name}!"
-    session[:intended_url] = nil
-  end
-
-  def handle_failed_authentication
-    flash.now[:alert] = 'Invalid email/password combination!'
-    render :new, status: :unprocessable_entity
+    user ? successful_authentication(user) : failed_authentication
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to movies_url, status: :see_other,
                             notice: "You're now signed out!"
+  end
+
+  private
+
+  def authenticate_user
+    User.find_by(email: params[:email])&.authenticate(params[:password])
+  end
+
+  def successful_authentication(user)
+    session[:user_id] = user.id
+    redirect_to (session[:intended_url] || user),
+                notice: "Welcome back, #{user.name}!"
+  end
+
+  def failed_authentication
+    flash.now[:alert] = 'Invalid email/password combination!'
+    render :new, status: :unprocessable_entity
   end
 end
