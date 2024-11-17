ffc8d7122d20f1651fbacf003c48d01cdd0cb819
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-11-17 22:49:32 +0100

    Create migration to add the required columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_19_204348) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_17_212537) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -89,6 +89,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_19_204348) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.integer "pin"
+    t.boolean "verified", default: false
   end
 
   add_foreign_key "characterizations", "genres"
