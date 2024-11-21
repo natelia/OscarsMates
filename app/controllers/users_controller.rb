383ce9224324e2f36702c3a331b0ef9349e02dc5
Natalia <natalia.jadwiga.gajewska@gmail.com>
2024-11-21 22:34:45 +0100

    Trigger the Email After User Creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+       # Send the verification email
+      UserMailer.verification_email(@user).deliver_now
+
+       # Log the user in
       session[:user_id] = @user.id
+      
       redirect_to @user, notice: 'Thanks for signing up!'
     else
       render :new, status: :unprocessable_entity
