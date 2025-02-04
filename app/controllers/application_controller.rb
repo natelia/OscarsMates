e0a5f14a0612dd311dee75602d76230d230757b0
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-02-04 22:23:55 +0100

    Verify and test cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id]) || User.find_by(id: cookies.signed[:user_id])
   end
 
   helper_method :current_user
