70f8ec7aa7401428256f4a24ee7fca2b4ba1e295
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-02-12 18:23:29 +0100

    Add index to watched_on and fixd the syntax

--- db/migrate/20250212164639_ensure_watched_on_not_null.rb
+++ db/migrate/20250212164639_ensure_watched_on_not_null.rb
@@ -3,9 +3,11 @@ class EnsureWatchedOnNotNull < ActiveRecord::Migration[7.1]
     Review.where(watched_on: nil).update_all("watched_on = created_at")
 
     change_column_null :reviews, :watched_on, false
+    add_index :reviews, :watched_on
+  end
 
   def down
+    remove_index :reviews, :watched_on
     change_column_null :reviews, :watched_on, true
-  end
   end
 end
