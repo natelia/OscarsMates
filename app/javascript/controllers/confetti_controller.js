9e70bee525a6c575fa73eb7689808c5369146422
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-02-25 21:29:11 +0100

    Create controller for confetti

---
+++ app/javascript/controllers/confetti_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+import confetti from 'canvas-confetti';
+
+export default class extends Controller {
+  static values = { enabled: Boolean }
+
+  connect() {
+    if (this.enabledValue) {
+      this.fireConfetti();
+    }
+  }
+
+  fireConfetti() {
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { y: 0.6 }
+    });
+  }
+} 
