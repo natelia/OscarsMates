6a9b1c3d2e2fc4b5689672cd85497a02805139a6
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-08-20 10:29:04 +0200

    Define query methods

--- app/queries/list_movies_query.rb
+++ app/queries/list_movies_query.rb
@@ -21,4 +21,19 @@ attr_reader :query, :filter, :current_user
   def prepare_collection
     @results = Movie.all
   end
+
+  def search_movies
+    @results = @results.where('title LIKE ?', "%#{query}%")
+  end
+
+  def sort_movies
+    @results = @results.order(:title)
+  end
+
+  def filter_movies
+    return @results unless current_user && filter == "unwatched"
+
+    reviewed_movie_ids = current_user.reviews.pluck(:movie_id)
+    @results = @results.where.not(id: reviewed_movie_ids)
+  end
 end
