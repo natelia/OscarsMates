0f0d2b7b7070f3878c435185e82f02a07075ff89
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-08-22 17:01:56 +0200

    Fix test setup: load fixtures for test environment

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class MovieTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is invalid without title" do
+  movie = Movie.new(
+    title: nil,
+    english_title: "English Title",
+    where_to_watch: "Cinema",
+    runtime: 120,
+    rating: 8,
+    url: "http://example.com",
+    picture_url: "http://example.com/img.jpg"
+  )
+  assert_not movie.valid?
+  assert_includes movie.errors[:title], "can't be blank"
+  end
 end
