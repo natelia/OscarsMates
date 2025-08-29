2ee06db8e09bbb57830cf9b0bd8c1353d5bec512
Natalia <natalia.jadwiga.gajewska@gmail.com>
2025-08-29 15:36:35 +0200

    More precise method naming, and make searching movies case-insensitive

--- test/helpers/movies_helper_test.rb
+++ test/helpers/movies_helper_test.rb
@@ -18,12 +18,12 @@ class MoviesHelperTest < ActionView::TestCase
     assert_includes result, '5.0 stars'
   end
 
-  def test_watched_button
+  def test_watch_button_text_for_movie_with_no_review
     result = watch_or_unwatch_button(@movie_no_reviews, nil)
     assert_includes result, 'Watched!'
   end
 
-  def test_unwatched_button
+  def test_unwatch_button_text_for_movie_with_no_review
     result = watch_or_unwatch_button(@movie_with_reviews, @review)
     assert_includes result, 'Unwatch!'
   end
